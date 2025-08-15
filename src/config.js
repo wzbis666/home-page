9ffcaaab990ff713c0e7dbba5a10ccc8726706ef
wzbis666 <3378621722@qq.com>
2025-08-15 11:28:35 +0800

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,9 +1,9 @@
 const config = {
 	//网页元数据
 	metaData: {
-		title: 'Leleo的个人主页🎉',
-		description: '欢迎来到Leleo的奇妙世界！',
-		keywords: 'Leleo,leleo,个人主页,个人网站',
+		title: 'LeoZi的个人主页🎉',
+		description: '欢迎来到LeoZi的奇妙世界！',
+		keywords: 'LeoZi,LeoZi,个人主页,个人网站',
 		icon: '/favicon.ico'   //网页图标，支持外链
 	},
 
